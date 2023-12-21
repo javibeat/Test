c7449749a834c9fd58b2fc79064732305951a7a4
javibeat <javibeat@gmail.com>
2023-12-21 15:16:19 +0400

    script

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Cargar el menú de menu.html
-  fetch('menu.html')
+    // Cargar el menú de menu.html
+    fetch('menu.html')
       .then(response => response.text())
       .then(data => {
-          document.querySelector('.menu-container').innerHTML = data;
+        document.querySelector('.menu-container').innerHTML = data;
       });
-
-  // Alternar el menú de hamburguesa
-  document.querySelector('.menu-hamburger').addEventListener('click', function() {
+  
+    // Alternar el menú de hamburguesa
+    document.querySelector('.menu-hamburger').addEventListener('click', function() {
       document.querySelector('.sideBar').classList.toggle('expanded');
-  });
-
-  // Cambiar entre modos claro y oscuro
-  document.querySelector('.mode').addEventListener('click', function() {
+    });
+  
+    // Cambiar entre modos claro y oscuro
+    document.querySelector('.mode').addEventListener('click', function(event) {
+      const modeIcon = document.querySelector('.mode i');
+      document.body.classList.toggle('dark-mode');
       if (document.body.classList.contains('dark-mode')) {
-          document.body.classList.remove('dark-mode');
-          document.querySelector('.mode i').classList.remove('fas', 'fa-sun');
-          document.querySelector('.mode i').classList.add('fas', 'fa-moon');
+        modeIcon.classList.remove('fa-moon');
+        modeIcon.classList.add('fa-sun');
       } else {
-          document.body.classList.add('dark-mode');
-          document.querySelector('.mode i').classList.remove('fas', 'fa-moon');
-          document.querySelector('.mode i').classList.add('fas', 'fa-sun');
+        modeIcon.classList.remove('fa-sun');
+        modeIcon.classList.add('fa-moon');
       }
+      event.stopPropagation(); // Prevenir que otros manejadores sean llamados
+    });
+  
+    // Prevenir que el cambio de modo se dispare al hacer clic en enlaces
+    document.querySelectorAll('.menu-container a').forEach(link => {
+      link.addEventListener('click', function(event) {
+        event.stopPropagation(); // Prevenir que el clic suba al .mode
+      });
+    });
   });
-});
