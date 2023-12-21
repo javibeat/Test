d6158b08c3e269530fd3b119a35ad4277c7f9fdf
javibeat <javibeat@gmail.com>
2023-12-21 14:17:06 +0400

    mode

--- script.js
+++ script.js
@@ -1,31 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Carga el menú desde menu.html
-    const menuContainer = document.getElementById('sidebar');
-    fetch('menu.html')
+  // Cargar el menú de menu.html
+  fetch('menu.html')
       .then(response => response.text())
       .then(data => {
-        menuContainer.innerHTML = data;
-        // Aquí también se podría inicializar el estado del modo oscuro/claro
+          document.querySelector('.menu-container').innerHTML = data;
       });
-  
-    // Función para alternar entre modos claro y oscuro
-function toggleMode() {
-    const body = document.body;
-    body.classList.toggle("dark-mode"); // Asumimos que existe una clase .dark-mode en el CSS
-    // Guardar la preferencia en localStorage
-    localStorage.setItem('mode', body.classList.contains("dark-mode") ? "dark" : "light");
-  }
-  
-  // Aplicar la preferencia guardada al cargar la página
-  window.onload = () => {
-    const savedMode = localStorage.getItem('mode');
-    if (savedMode === "dark") {
-      document.body.classList.add("dark-mode");
-    }
-  };
-  
-  // Añadir el evento para cambiar el modo al botón correspondiente
-  document.querySelector(".mode-button").addEventListener("click", toggleMode);
-  
+
+  // Alternar el menú de hamburguesa
+  document.querySelector('.menu-hamburger').addEventListener('click', function() {
+      document.querySelector('.sideBar').classList.toggle('expanded');
   });
-  
+
+  // Cambiar entre modos claro y oscuro
+  document.querySelector('.mode').addEventListener('click', function() {
+      document.body.classList.toggle('dark-mode');
+  });
+});
