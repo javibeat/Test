c40087818226532b6cc4059dac3f7c34cbcf327b
javibeat <javibeat@gmail.com>
2023-12-21 16:54:07 +0400

    cambios

--- script.js
+++ script.js
@@ -1,35 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const body = document.body;
-    
-    function applyTheme() {
-      const theme = localStorage.getItem('theme');
-      if (theme === 'dark') {
-        body.classList.add('dark-mode');
-      } else {
-        body.classList.remove('dark-mode');
-      }
-      updateModeIcon();
-    }
-  
-    function updateModeIcon() {
-      const modeSwitch = document.querySelector('.mode');
-      const icon = modeSwitch.querySelector('i');
-      if (body.classList.contains('dark-mode')) {
-        icon.classList.remove('fa-moon');
-        icon.classList.add('fa-sun');
-      } else {
-        icon.classList.add('fa-moon');
-        icon.classList.remove('fa-sun');
-      }
-    }
-  
+document.addEventListener('DOMContentLoaded', () => {
+    const menuContainer = document.getElementById('menuContainer');
+    const menuHamburger = document.getElementById('menuHamburger');
+    const sideBar = document.querySelector('.sideBar');
     const modeSwitch = document.querySelector('.mode');
-    modeSwitch.addEventListener('click', function () {
-      body.classList.toggle('dark-mode');
-      localStorage.setItem('theme', body.classList.contains('dark-mode') ? 'dark' : 'light');
-      updateModeIcon();
+
+    // Cargar el menú desde menu.html
+    fetch('menu.html')
+        .then(response => response.text())
+        .then(html => {
+            menuContainer.innerHTML = html;
+        })
+        .catch(error => console.error('Error al cargar el menú:', error));
+
+    // Funcionalidad del menú hamburguesa
+    menuHamburger.addEventListener('click', () => {
+        sideBar.classList.toggle('expanded');
     });
-  
-    // Aplica el tema al cargar la página
-    applyTheme();
-  });
+
+    // Funcionalidad para cambiar modos claro/oscuro
+    modeSwitch.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+});
