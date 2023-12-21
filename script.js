339227599e6a8eed72476c12a0b4b88411f8f505
javibeat <javibeat@gmail.com>
2023-12-21 09:40:29 +0400

    true

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 // Función para cambiar entre modo claro y oscuro
 function toggleMode() {
     var body = document.body;
+    // Alternar las clases para el modo
     body.classList.toggle("light-mode");
     body.classList.toggle("dark-mode");
+
+    // Guardar la preferencia en localStorage
+    if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark-mode");
+    } else {
+        localStorage.setItem("theme", "light-mode");
+    }
+}
+
+// Función para cargar el modo guardado
+function loadPreferredMode() {
+    var savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme) {
+        document.body.className = savedTheme;
+    }
 }
 
 // Cargar el menú desde menu.html
@@ -10,10 +27,16 @@ function loadMenu() {
     fetch('menu.html')
         .then(response => response.text())
         .then(data => {
-            document.getElementById('menu-container').innerHTML = data;
+            var menuContainer = document.querySelector('.menu-container');
+            if(menuContainer) {
+                menuContainer.innerHTML = data;
+            }
         })
         .catch(err => console.error(err));
 }
 
-// Inicializar la carga del menú
-loadMenu();
+// Inicializar la carga del menú y el modo preferido
+document.addEventListener('DOMContentLoaded', () => {
+    loadMenu();
+    loadPreferredMode();
+});
