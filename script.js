1e52f9b2933ab494e44384fd3c3fa77b6b3a6fea
javibeat <javibeat@gmail.com>
2023-12-21 16:35:20 +0400

    SCRIPT

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const modeSwitch = document.querySelector('.mode');
-  const body = document.body;
-
-  // Function to toggle the theme icon
-  function toggleModeIcon() {
-    const icon = modeSwitch.querySelector('i');
-    if (body.classList.contains('dark-mode')) {
-      icon.classList.remove('fa-moon');
-      icon.classList.add('fa-sun');
-    } else {
-      icon.classList.add('fa-moon');
-      icon.classList.remove('fa-sun');
+    const body = document.body;
+    
+    function applyTheme() {
+      const theme = localStorage.getItem('theme');
+      if (theme === 'dark') {
+        body.classList.add('dark-mode');
+      } else {
+        body.classList.remove('dark-mode');
+      }
+      updateModeIcon();
     }
-  }
-
-  // Event to change the mode and icon
-  modeSwitch.addEventListener('click', function () {
-    body.classList.toggle('dark-mode');
-    toggleModeIcon();
-    localStorage.setItem('theme', body.classList.contains('dark-mode') ? 'dark' : 'light');
+  
+    function updateModeIcon() {
+      const modeSwitch = document.querySelector('.mode');
+      const icon = modeSwitch.querySelector('i');
+      if (body.classList.contains('dark-mode')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+      } else {
+        icon.classList.add('fa-moon');
+        icon.classList.remove('fa-sun');
+      }
+    }
+  
+    const modeSwitch = document.querySelector('.mode');
+    modeSwitch.addEventListener('click', function () {
+      body.classList.toggle('dark-mode');
+      localStorage.setItem('theme', body.classList.contains('dark-mode') ? 'dark' : 'light');
+      updateModeIcon();
+    });
+  
+    // Aplica el tema al cargar la página
+    applyTheme();
   });
-
-  // Load the saved theme from local storage
-  if (localStorage.getItem('theme') === 'dark') {
-    body.classList.add('dark-mode');
-  }
-  toggleModeIcon();
-});
