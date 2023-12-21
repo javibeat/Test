993c57e8addc0dacde855439c3623924819650c7
javibeat <javibeat@gmail.com>
2023-12-21 17:24:12 +0400

    cambios

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuContainer = document.getElementById('menuContainer');
     const menuHamburger = document.getElementById('menuHamburger');
     const sideBar = document.querySelector('.sideBar');
-    const modeSwitch = document.querySelector('.mode');
+    const modeSwitch = document.getElementById('modeSwitch'); // Asegúrate de que el ID es correcto
 
     // Cargar el menú desde menu.html
     fetch('menu.html')
@@ -12,13 +12,24 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .catch(error => console.error('Error al cargar el menú:', error));
 
-    // Funcionalidad del menú hamburguesa
+    // Funcionalidad del menú hamburguesa para móviles
     menuHamburger.addEventListener('click', () => {
+        // Toggle la clase 'expanded' para el sidebar
         sideBar.classList.toggle('expanded');
+        // Comprobar si el menú está expandido para mostrar u ocultar el menú
+        const menu = sideBar.querySelector('.menu'); // Selecciona el menú dentro del sidebar
+        if (menu) {
+            if (sideBar.classList.contains('expanded')) {
+                menu.style.display = 'flex'; // o 'block', dependiendo de tus estilos
+            } else {
+                menu.style.display = 'none';
+            }
+        }
     });
 
     // Funcionalidad para cambiar modos claro/oscuro
     modeSwitch.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
+        // Aquí puedes cambiar las variables de CSS si es necesario
     });
 });
