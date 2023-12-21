6eccc42382c60b1491dcb871ffdf5dc52409541c
javibeat <javibeat@gmail.com>
2023-12-21 12:25:59 +0400

    re test

--- script.js
+++ script.js
@@ -1,52 +1,31 @@
-// Función para cambiar entre modo claro y oscuro
-function toggleMode() {
-    var body = document.body;
-    var modeIcon = document.getElementById('mode-icon'); // Añade el ID 'mode-icon' al ícono correspondiente en tu HTML
-
-    body.classList.toggle('light-mode');
-    body.classList.toggle('dark-mode');
-
-    if (body.classList.contains('dark-mode')) {
-        modeIcon.className = 'fas fa-moon';
-        localStorage.setItem('theme', 'dark-mode');
-    } else {
-        modeIcon.className = 'fas fa-sun';
-        localStorage.setItem('theme', 'light-mode');
-    }
-}
-
-function toggleMobileMenu() {
-    var menuContainer = document.querySelector('.menu-container');
-    menuContainer.classList.toggle('active');
-}
-
-// Función para cargar el modo guardado
-function loadPreferredMode() {
-    var savedTheme = localStorage.getItem('theme');
-    var modeIcon = document.getElementById('mode-icon');
-    var body = document.body;
-
-    if (savedTheme === 'dark-mode') {
-        body.className = 'dark-mode';
-        modeIcon.className = 'fas fa-moon';
-    } else {
-        body.className = 'light-mode';
-        modeIcon.className = 'fas fa-sun';
-    }
-}
-
-// Cargar el menú desde menu.html
-function loadMenu() {
+document.addEventListener("DOMContentLoaded", function() {
+    // Carga el menú desde menu.html
+    const menuContainer = document.getElementById('sidebar');
     fetch('menu.html')
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector('.menu-container').innerHTML = data;
-        })
-        .catch(err => console.error(err));
-}
-
-// Evento para cargar el menú y el modo preferido al cargar el documento
-document.addEventListener('DOMContentLoaded', () => {
-    loadMenu();
-    loadPreferredMode();
-});
+      .then(response => response.text())
+      .then(data => {
+        menuContainer.innerHTML = data;
+        // Aquí también se podría inicializar el estado del modo oscuro/claro
+      });
+  
+    // Función para alternar entre modos claro y oscuro
+function toggleMode() {
+    const body = document.body;
+    body.classList.toggle("dark-mode"); // Asumimos que existe una clase .dark-mode en el CSS
+    // Guardar la preferencia en localStorage
+    localStorage.setItem('mode', body.classList.contains("dark-mode") ? "dark" : "light");
+  }
+  
+  // Aplicar la preferencia guardada al cargar la página
+  window.onload = () => {
+    const savedMode = localStorage.getItem('mode');
+    if (savedMode === "dark") {
+      document.body.classList.add("dark-mode");
+    }
+  };
+  
+  // Añadir el evento para cambiar el modo al botón correspondiente
+  document.querySelector(".mode-button").addEventListener("click", toggleMode);
+  
+  });
+  
