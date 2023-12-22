2007211f05d65119492c6f75d0bf5c0cd0ee67ed
javibeat <javibeat@gmail.com>
2023-12-22 11:12:42 +0400

    cambios

--- script.js
+++ script.js
@@ -2,33 +2,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const sideBar = document.querySelector('.sideBar');
     const menuHamburger = document.querySelector('.menu-hamburger');
     const modeSwitch = document.querySelector('.mode');
+    const menuContainer = document.querySelector('.menu-container');
 
-    // Verificar si el usuario tiene una preferencia de modo claro/oscuro guardada
-    if(localStorage.getItem('darkMode') === 'true') {
-        document.body.classList.add('dark-mode');
-        modeSwitch.innerHTML = '<i class="fas fa-sun"></i>'; // Cambiar a ícono de sol si está en modo oscuro
-    } else {
-        modeSwitch.innerHTML = '<i class="fas fa-moon"></i>'; // Ícono de luna para modo claro
+    // Función para cambiar el icono de modo y guardar preferencia
+    function toggleDarkMode() {
+        const isDarkMode = document.body.classList.toggle('dark-mode');
+        localStorage.setItem('darkMode', isDarkMode);
+        modeSwitch.className = isDarkMode ? 'mode fas fa-sun' : 'mode fas fa-moon';
     }
 
-    modeSwitch.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-        // Guardar la preferencia de modo del usuario
-        let isDarkMode = document.body.classList.contains('dark-mode');
-        localStorage.setItem('darkMode', isDarkMode);
-        modeSwitch.innerHTML = isDarkMode ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
+    // Establecer el modo y el icono inicial al cargar la página
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    document.body.classList.toggle('dark-mode', isDarkMode);
+    modeSwitch.className = isDarkMode ? 'mode fas fa-sun' : 'mode fas fa-moon';
+
+    modeSwitch.addEventListener('click', toggleDarkMode);
+
+    menuHamburger.addEventListener('click', () => {
+        sideBar.classList.toggle('expanded');
+        menuContainer.style.display = sideBar.classList.contains('expanded') ? 'block' : 'none';
     });
 
+    // Cargar el menú desde menu.html
     fetch('menu.html')
         .then(response => response.text())
         .then(html => {
-            const menuContainer = document.querySelector('.menu-container');
             menuContainer.innerHTML = html;
-            // Resto del código...
         })
         .catch(error => {
             console.error('Error al cargar el menú:', error);
         });
-
-    // Resto del código...
 });
